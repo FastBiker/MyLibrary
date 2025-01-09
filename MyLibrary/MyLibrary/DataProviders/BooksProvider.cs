fe11b6d674ca91907450be4ccad4ebc8e03b6c6b
Piotr Dyduch <padyduch@o2.pl>
2025-01-09 16:05:54 +0100

    Pierwsze metody z użyciem LINQ => Selecy()

---
+++ MyLibrary/MyLibrary/DataProviders/BooksProvider.cs
@@ -0,0 +1,40 @@
+﻿using MyLibrary.Entities;
+using MyLibrary.Repositories;
+
+namespace MyLibrary.DataProviders;
+
+public class BooksProvider : IBooksProvider
+{
+    private readonly IRepository<Book> _bookRepository;
+    public BooksProvider(IRepository<Book> bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+    public string AnonimousClass()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Book> FilterBooks(int minPage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal GetMinimumPriceOffAllBooks()
+    {
+        var books = _bookRepository.GetAll();
+        return (decimal)books.Select(x => x.Price).Min();
+    }
+
+    public List<Book> GetSpecificColumns()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<string> GetUniqueBookOwners()
+    {
+        var books = _bookRepository.GetAll();
+        var owners = books.Select(x => x.Owner).Distinct().ToList();
+        return owners;
+    }
+}
--- MyLibrary/MyLibrary/DataProviders/BooksProviderBasic.cs
+++ MyLibrary/MyLibrary/DataProviders/BooksProviderBasic.cs
@@ -1,70 +1,70 @@
-﻿using MyLibrary.Entities;
-using MyLibrary.Repositories;
+﻿//using MyLibrary.Entities;
+//using MyLibrary.Repositories;
 
-namespace MyLibrary.DataProviders;
+//namespace MyLibrary.DataProviders;
 
-public class BooksProviderBasic : IBooksProvider
-{
-    private readonly IRepository<Book> _bookRepository;
-    public BooksProviderBasic(IRepository<Book> bookRepository)
-    {
-        _bookRepository = bookRepository;
-    }
+//public class BooksProviderBasic : IBooksProvider
+//{
+//    private readonly IRepository<Book> _bookRepository;
+//    public BooksProviderBasic(IRepository<Book> bookRepository)
+//    {
+//        _bookRepository = bookRepository;
+//    }
 
-    public string AnonimousClass()
-    {
-        throw new NotImplementedException();
-    }
+//    public string AnonimousClass()
+//    {
+//        throw new NotImplementedException();
+//    }
 
-    public List<Book> FilterBooks(int minPagesNumber)
-    {
-        var books = _bookRepository.GetAll();
-        var list = new List<Book>();
+//    public List<Book> FilterBooks(int minPagesNumber)
+//    {
+//        var books = _bookRepository.GetAll();
+//        var list = new List<Book>();
 
-        foreach (var book in books)
-        {
-            if (book.PageNumber > minPagesNumber)
-            {
-                list.Add(book);
-            }
-        }
+//        foreach (var book in books)
+//        {
+//            if (book.PageNumber > minPagesNumber)
+//            {
+//                list.Add(book);
+//            }
+//        }
 
-        return list;
-    }
+//        return list;
+//    }
 
-    public decimal GetMinimumPriceOffAllBooks()
-    {
-        var books = _bookRepository.GetAll();
-        decimal minPrice = decimal.MaxValue;
-        foreach (var book in books)
-        {
-            if (book.Price < minPrice)
-            {
-                minPrice = (decimal)book.Price;
-            }
-        }
+//    public decimal GetMinimumPriceOffAllBooks()
+//    {
+//        var books = _bookRepository.GetAll();
+//        decimal minPrice = decimal.MaxValue;
+//        foreach (var book in books)
+//        {
+//            if (book.Price < minPrice)
+//            {
+//                minPrice = (decimal)book.Price;
+//            }
+//        }
 
 
-        return minPrice;
-    }
+//        return minPrice;
+//    }
 
-    public List<Book> GetSpecificColumns()
-    {
-        throw new NotImplementedException();
-    }
+//    public List<Book> GetSpecificColumns()
+//    {
+//        throw new NotImplementedException();
+//    }
 
-    public List<string> GetUniqueBookOwners()
-    {
-        var books = _bookRepository.GetAll();
-        var list = new List<string>();
-        foreach (var book in books)
-        {
-            if (!list.Contains(book.Owner))
-            {
-                list.Add(book.Owner);
-            }
-        }
+//    public List<string> GetUniqueBookOwners()
+//    {
+//        var books = _bookRepository.GetAll();
+//        var list = new List<string>();
+//        foreach (var book in books)
+//        {
+//            if (!list.Contains(book.Owner))
+//            {
+//                list.Add(book.Owner);
+//            }
+//        }
 
-        return list;
-    }
-}
+//        return list;
+//    }
+//}
