ea1f42af85b1a5e00e5ff7bc001dd47e528c016d
Piotr Dyduch <padyduch@o2.pl>
2025-02-10 16:57:16 +0100

    Włąsny Reader do plikó CSV bez "yield"

--- MyLibrary/MyLibrary/Components/CsvReader/Extensions/BooksExtensions.cs
+++ MyLibrary/MyLibrary/Components/CsvReader/Extensions/BooksExtensions.cs
@@ -7,12 +7,13 @@ public static class BooksExtensions
 {
     public static IEnumerable<RealBook> ToBook(this IEnumerable<string> source)
     {
+        var books = new List<RealBook>();
 
         foreach (var line in source)
         {
             var columns = line.Split(';');
 
-            yield return new RealBook
+            var book = new RealBook
             {
                 Lp = int.Parse(columns[0]),
                 AuthorName = columns[1],
@@ -24,6 +25,27 @@ public static class BooksExtensions
                 Price = decimal.Parse(columns[7]),
                 Comments = columns[8]
             };
+            books.Add(new RealBook());
         }
+
+        return books;
+
+        //foreach (var line in source)
+        //{
+        //    var columns = line.Split(';');
+
+        //    yield return new RealBook
+        //    {
+        //        Lp = int.Parse(columns[0]),
+        //        AuthorName = columns[1],
+        //        AuthorSurname = columns[2],
+        //        CollectiveAuthor = columns[3],
+        //        Title = columns[4],
+        //        PlaceInLibrary = columns[5],
+        //        IsForSale = bool.Parse(columns[6]),
+        //        Price = decimal.Parse(columns[7]),
+        //        Comments = columns[8]
+        //    };
+        //}
     }
 }
